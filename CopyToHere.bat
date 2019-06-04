ea6e4ec0619f01b50fbb746db43bcae15a1e87e9
Ent233 <t6478990390@gmail.com>
2019-06-03 21:02:41 -0400

    First version

---
+++ CopyToHere.bat
@@ -0,0 +1,14 @@
+@echo off
+setlocal EnableDelayedExpansion
+git pull origin master
+set DIR="%D:\workspace\Leetcode-workplace\%"
+for /r %DIR% %%i in (*.java) do (
+	set str=%%~pdi
+	set str=!str:~32!
+	set str=!str:~0,-5!
+	copy %%i .\Solutions\!str!.java
+)
+git add .
+git commit -m "Auto Update@%date% %time%"
+git push origin master
+pause
